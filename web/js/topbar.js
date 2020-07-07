c68a2f19abfb16848f2575142786d8e3a77bfa49
谢泽华 <756942648@qq.com>
2020-07-08 00:14:27 +0800

    在注销、新建简历、保存简历、发布工作按钮增加了二次确认的提示框

--- web/js/topbar.js
+++ web/js/topbar.js
@@ -1,11 +1,17 @@
 // 注销功能
-function logOut () {
-  axios
-    .put("/logOut")
-    .then(function (response){
-      window.location.href="index.html"
-    })
-    .catch(function (error) {
-      console.log(error.message)
-    })
+function logOut ()
+{
+  var zhuxiao=confirm("确定注销吗？")
+  if(zhuxiao==true)
+  {
+    axios
+        .put("/logOut")
+        .then(function (response)
+        {
+          window.location.href="index.html"
+        })
+        .catch(function (error) {
+          console.log(error.message)
+        })
+  }
 }
